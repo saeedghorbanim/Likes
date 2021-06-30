b0696db4975700c7b43fcd0fa818822500b1b2b4
saeedghorbanim <animation10733@yahoo.com>
2021-06-29 20:35:35 -0500

    Add files via upload

--- like2.js
+++ like2.js
@@ -3,15 +3,25 @@ var counting = document.querySelector("#likes1");
 function add ()
 {
     count++;
-    counting.innerText = count + " like(s)"
+    counting.innerText = count + " like(s)";
     console.log(count);
 }
 
-var count2=12;
+
+var count2 = 12;
 var counting2 = document.querySelector("#likes2");
 function add2 ()
 {
     count2++;
-    counting2.innerText = count2 + " like(s)"
+    counting2.innerText = count2 + " like(s)";
     console.log(count2);
+}
+
+var count3 = 9;
+var counting3 = document.querySelector("#likes3");
+function add3 ()
+{
+    count3++;
+    counting3.innerText = count3 + " like(s)";
+    console.log(count3);
 }
